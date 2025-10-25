01149b29121c286644eee14b32e1ff8dbdb40be9
yunajoe <yunajoe@gmail.com>
2025-10-25 09:28:59 +0900

    fix(test): 우승자를 출력하는 로직 테스팅 버그 수정

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -24,7 +24,7 @@ const getLogSpy = () => {
 };
 
 describe('자동차 경주', () => {
-  test('우승자가 한명일 경우', async () => {
+  test('우승자가 한명일 경우 - 시도 횟수를 1번 했을 경우', async () => {
     // given
     const MOVING_FORWARD = 4;
     const STOP = 3;
@@ -44,10 +44,45 @@ describe('자동차 경주', () => {
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
     });
   });
-  test('우승자가 2명이상일 경우', async () => {
+  test('우승자가 한명일 경우 - 시도 횟수 2번 이상했을 경우', async () => {
+    // given
     const MOVING_FORWARD = 4;
     const STOP = 3;
+    const inputs = ['pobi,woni', '2'];
+    const logs = [
+      'pobi : -',
+      'woni : ',
+      'pobi : --',
+      'woni : -',
+      '최종 우승자 : pobi',
+    ];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputs);
+    mockRandoms([MOVING_FORWARD, STOP, MOVING_FORWARD, MOVING_FORWARD]);
+
+    // when
+    const app = new App();
+    await app.run();
+
+    // then
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+  test('우승자가 2명이상일 경우', async () => {
+    const MOVING_FORWARD = 5;
+    const STOP = 0;
     const inputs = ['car1,car2,car3', '2'];
+    const logs = [
+      'car1 : -',
+      'car2 : -',
+      'car3 : ',
+      'car1 : --',
+      'car2 : --',
+      'car3 : -',
+      '최종 우승자 : car1, car2',
+    ];
 
     const logSpy = getLogSpy();
 
@@ -57,28 +92,10 @@ describe('자동차 경주', () => {
       MOVING_FORWARD,
       STOP,
       MOVING_FORWARD,
-      STOP,
       MOVING_FORWARD,
-      STOP,
       MOVING_FORWARD,
-      STOP,
     ]);
-    const logs = [
-      '\n',
-      '실행 결과',
-      'car1 : -',
-      'car2 : -',
-      'car3 : ',
-      '\n',
-      'car1 : --',
-      'car2 : -',
-      'car3 : -',
-      '\n',
-      'car1 : --',
-      'car2 : --',
-      'car3 : -',
-      '최종 우승자 : car1, car2',
-    ];
+
     const app = new App();
     await app.run();
     logs.forEach((log) => {
